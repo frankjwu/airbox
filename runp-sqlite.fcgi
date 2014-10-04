6a2cc8e62ac899c6874c83acacab4462d6aa65ec
frankjwu <me@frankjwu.com>
2014-10-04 15:40:22 -0400

    clean

--- runp-sqlite.fcgi
+++
@@ -1,6 +0,0 @@
-#!flask/bin/python
-from flipflop import WSGIServer
-from app import app
-
-if __name__ == '__main__':
-    WSGIServer(app).run()
--- tr_compile.py
+++
@@ -1,8 +0,0 @@
-#!flask/bin/python
-import os
-import sys
-if sys.platform == 'win32':
-    pybabel = 'flask\\Scripts\\pybabel'
-else:
-    pybabel = 'flask/bin/pybabel'
-os.system(pybabel + ' compile -d app/translations')
