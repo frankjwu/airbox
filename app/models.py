8caec893b197e2276fcb39b997f36e6acc42372f
frankjwu <me@frankjwu.com>
2014-10-04 16:50:49 -0400

    Models fixed

--- app/models.py
+++ app/models.py
@@ -9,10 +9,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     transactions = db.relationship('Transaction')
+    type = db.Column(db.String(50))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'user',
+        'polymorphic_on':type
+    }
 
 class Seller(User):
-    __tablename__ = 'user'
-    __mapper_args__ = { 'polymorphic_identity': 'user' }
+    __tablename__ = 'seller'
+    __mapper_args__ = { 'polymorphic_identity': 'seller' }
 
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     space_selling = db.Column(db.Float)
@@ -25,15 +31,15 @@ class Seller(User):
         return space
 
 class Buyer(User):
-    __tablename__ = 'user'
-    __mapper_args__ = { 'polymorphic_identity': 'user' }
+    __tablename__ = 'buyer'
+    __mapper_args__ = { 'polymorphic_identity': 'buyer' }
 
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Float)
-    total_size = db.column(db.Float)
+    total_size = db.Column(db.Float)
     timestamp = db.Column(db.DateTime)
-    sellers = db.Column(db.PickleType(mutable=True)) # List of sellers that the space is split between
+    sellers = db.Column(db.PickleType) # List of sellers that the space is split between
     buyer_id = db.Column(db.Integer, db.ForeignKey('buyer.id')) # One buyer
