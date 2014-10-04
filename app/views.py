3bcaf1749fe5be339446a6e393bb541b8255632e
frankjwu <me@frankjwu.com>
2014-10-04 17:24:41 -0400

    Changing keys + dropbox auth flow start

--- app/views.py
+++ app/views.py
@@ -1,34 +1,35 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, session
 import os
 import dropbox
 
-DROPBOX_BOX_KEY = os.environ.get('DROPBOX_BOX_KEY')
-DROPBOX_APP_SECRET = os.environ.get('DROPBOX_APP_SECRET')
-flow = dropbox.client.DropboxOAuth2FlowNoRedirect(DROPBOX_BOX_KEY, DROPBOX_APP_SECRET)
+AIRBOX_DROPBOX_APP_KEY = os.environ.get('AIRBOX_DROPBOX_APP_KEY')
+AIRBOX_DROPBOX_APP_SECRET = os.environ.get('AIRBOX_DROPBOX_APP_SECRET')
 
 @app.route('/')
-# def home():
-# 	return redirect(url_for('dropbox_auth_start'))	
+def home():
+  if not 'access_token' in session:
+    return redirect(url_for('dropbox_auth_start'))
+  return 'Authenticated.'
 
 @app.route('/index')
 def index():
 	return render_template('index.html')
 
-# @app.route('/dropbox-auth-start')
-# def dropbox_auth_start():
-# 	return redirect(get_auth_flow().start())
+@app.route('/authenticate')
+def dropbox_auth_start():
+	return redirect(get_auth_flow().start())
 
-# @app.route('/dropbox-auth-finish')
-# def dropbox_auth_finish():
-# 	try:
-# 		access_token, user_id, url_state = get_auth_flow().finish(request.args)
-# 	except:
-# 		abort(400)
-# 	else:
-# 		session['access_token'] = access_token
-# 	return redirect(url_for('home'))
+@app.route('/authenticate-finish')
+def dropbox_auth_finish():
+	try:
+		access_token, user_id, url_state = get_auth_flow().finish(request.args)
+	except:
+		abort(400)
+	else:
+		session['access_token'] = access_token
+	return redirect(url_for('home'))
 
-# def get_auth_flow():
-# 	redirect_uri = url_for('dropbox_auth_finish', _external=True)
-# 	return DropboxOAuth2Flow(DROPBOX_APP_KEY, DROPBOX_APP_SECRET, redirect_uri, session, 'dropbox-auth-csrf-token')
+def get_auth_flow():
+	redirect_uri = url_for('dropbox_auth_finish', _external=True)
+	return dropbox.client.DropboxOAuth2Flow(AIRBOX_DROPBOX_APP_KEY, AIRBOX_DROPBOX_APP_SECRET, redirect_uri, session, 'dropbox-auth-csrf-token')
--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 #!flask/bin/python
 from app import app
+
+app.secret_key = ENV['AIRBOX_USER_SECRET_KEY']
+
 app.run(debug=True)
