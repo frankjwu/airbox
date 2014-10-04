4cd4940309d334d3db7a7a366bf929de6970a497
mhw32 <mike.wu@yale.edu>
2014-10-04 16:19:59 -0400

    environment variables

--- app/models.py
+++ app/models.py
@@ -2,15 +2,8 @@ from hashlib import md5
 import re
 from app import db
 from app import app
-from config import WHOOSH_ENABLED
 
 import sys
-if sys.version_info >= (3, 0):
-    enable_search = False
-else:
-    enable_search = WHOOSH_ENABLED
-    if enable_search:
-        import flask.ext.whooshalchemy as whooshalchemy
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,12 +12,6 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
-    followed = db.relationship('User',
-                               secondary=followers,
-                               primaryjoin=(followers.c.follower_id == id),
-                               secondaryjoin=(followers.c.followed_id == id),
-                               backref=db.backref('followers', lazy='dynamic'),
-                               lazy='dynamic')
 
     @staticmethod
     def make_valid_nickname(nickname):
@@ -61,26 +48,6 @@ class User(db.Model):
         return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % \
             (md5(self.email.encode('utf-8')).hexdigest(), size)
 
-    def follow(self, user):
-        if not self.is_following(user):
-            self.followed.append(user)
-            return self
-
-    def unfollow(self, user):
-        if self.is_following(user):
-            self.followed.remove(user)
-            return self
-
-    def is_following(self, user):
-        return self.followed.filter(
-            followers.c.followed_id == user.id).count() > 0
-
-    def followed_posts(self):
-        return Post.query.join(
-            followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.follower_id == self.id).order_by(
-                    Post.timestamp.desc())
-
     def __repr__(self):  # pragma: no cover
         return '<User %r>' % (self.nickname)
 
@@ -97,6 +64,3 @@ class Post(db.Model):
     def __repr__(self):  # pragma: no cover
         return '<Post %r>' % (self.body)
 
-
-if enable_search:
-    whooshalchemy.whoosh_index(app, Post)
