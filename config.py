8f5a58dea6430d1fc77fe66436628e66d082c369
mhw32 <mike.wu@yale.edu>
2014-10-04 17:28:43 -0400

    initial stuff on form

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
