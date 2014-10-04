5a8ec69a4152b730e7b78d2fb41f2965ae89047e
frankjwu <me@frankjwu.com>
2014-10-04 16:20:17 -0400

    models + basic functions

--- app/models.py
+++ app/models.py
@@ -14,89 +14,36 @@ else:
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
-    about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime)
-    followed = db.relationship('User',
-                               secondary=followers,
-                               primaryjoin=(followers.c.follower_id == id),
-                               secondaryjoin=(followers.c.followed_id == id),
-                               backref=db.backref('followers', lazy='dynamic'),
-                               lazy='dynamic')
+    transactions = db.relationship('Transaction')
 
-    @staticmethod
-    def make_valid_nickname(nickname):
-        return re.sub('[^a-zA-Z0-9_\.]', '', nickname)
+class Seller(User):
+    __tablename__ = 'user'
+    __mapper_args__ = { 'polymorphic_identity': 'user' }
 
-    @staticmethod
-    def make_unique_nickname(nickname):
-        if User.query.filter_by(nickname=nickname).first() is None:
-            return nickname
-        version = 2
-        while True:
-            new_nickname = nickname + str(version)
-            if User.query.filter_by(nickname=new_nickname).first() is None:
-                break
-            version += 1
-        return new_nickname
+    id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    space_selling = Column(db.Float)
 
-    def is_authenticated(self):
-        return True
+    def space_left(self):
+        space = self.space_selling
+        transactions = self.transactions
+        for t in transactions:
+            space -= t.total_size
+        return space
 
-    def is_active(self):
-        return True
+class Buyer(User):
+    __tablename__ = 'user'
+    __mapper_args__ = { 'polymorphic_identity': 'user' }
 
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        try:
-            return unicode(self.id)  # python 2
-        except NameError:
-            return str(self.id)  # python 3
-
-    def avatar(self, size):
-        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % \
-            (md5(self.email.encode('utf-8')).hexdigest(), size)
-
-    def follow(self, user):
-        if not self.is_following(user):
-            self.followed.append(user)
-            return self
-
-    def unfollow(self, user):
-        if self.is_following(user):
-            self.followed.remove(user)
-            return self
-
-    def is_following(self, user):
-        return self.followed.filter(
-            followers.c.followed_id == user.id).count() > 0
-
-    def followed_posts(self):
-        return Post.query.join(
-            followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.follower_id == self.id).order_by(
-                    Post.timestamp.desc())
-
-    def __repr__(self):  # pragma: no cover
-        return '<User %r>' % (self.nickname)
+    id = Column(Integer, ForeignKey('user.id'), primary_key=True)
 
 
-class Post(db.Model):
-    __searchable__ = ['body']
-
+class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
+    price = db.Column(db.Float)
+    total_size = db.column(db.Float)
+
     timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    language = db.Column(db.String(5))
+    sellers = db.Column(db.PickleType(mutable=True)) # List of sellers that the space is split between
+    buyer_id = db.Column(db.Integer, db.ForeignKey('buyer.id')) # One buyer
 
-    def __repr__(self):  # pragma: no cover
-        return '<Post %r>' % (self.body)
-
-
-if enable_search:
-    whooshalchemy.whoosh_index(app, Post)
