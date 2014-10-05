b63784960b1c836019001645ced207ef4a408bfe
frankjwu <me@frankjwu.com>
2014-10-05 09:14:36 -0400

    guni

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run:app --log-file=-
+web: gunicorn webstart:app --log-file=-
--- run.py
+++ run.py
@@ -3,7 +3,3 @@
 
 from app import app
 app.run(debug=True)
-
-if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port)
