2350ec81873515d66046f77e2168b0daa9188bf8
frankjwu <me@frankjwu.com>
2014-10-05 04:21:56 -0400

    friendly name

--- app/views.py
+++ app/views.py
@@ -122,7 +122,32 @@ def current_access_token():
 	else:
 		return None
 
-def generate_friendly_names():
+def generate_friendly_name()
+	# https://gist.github.com/hemanth/3205763
+  # example output: "falling-late-violet-forest-d27b3"
+  adjs = [ "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
+        "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
+        "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
+        "billowing", "broken", "cold", "damp", "falling", "frosty", "green",
+        "long", "late", "lingering", "bold", "little", "morning", "muddy", "old",
+        "red", "rough", "still", "small", "sparkling", "throbbing", "shy",
+        "wandering", "withered", "wild", "black", "young", "holy", "solitary",
+        "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine",
+        "polished", "ancient", "purple", "lively", "nameless"
+    ]
+  nouns = [ "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
+        "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
+        "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
+        "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
+        "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
+        "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
+        "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
+        "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper",
+        "frog", "smoke", "star"
+    ]
+  hex = "0123456789abcdef"
+  return (random.choice(adjs) + "-" + random.choice(adjs) + "-" + random.choice(nouns) + "-" + random.choice(nouns) + "-" + 
+            random.choice(hex) + random.choice(hex) + random.choice(hex) + random.choice(hex) + random.choice(hex))
 	
 
 def upload_processor(upload):
@@ -132,8 +157,8 @@ def upload_processor(upload):
 
 	# 1. Encrypt file and store secret_key
 	text, key = encrypt_file(upload_file)
-	folder = Random.new().read(10) # TODO: take out invalid folder chars
-	name = Random.new().read(20) # TODO: take out invalid file chars
+	folder = generate_friendly_name()
+	name = generate_friendly_name()
 	enc_file = open("/tmp/" + name, 'wb')
 	enc_file.write(text)
 	enc_file.close()
@@ -160,7 +185,7 @@ def upload_processor(upload):
 
 		data = tmp.read(to_read) # Read next bytes (the block we want here)
 
-		response = client.put_file('/airbox' + folder + "/" + name, data)
+		response = client.put_file('/airbox-' + folder + "/" + name, data)
 		if not response:
 			return "Error"
 
