8117e82fd90155de4b7fecbcf5b5e7e213e89413
mhw32 <mike.wu@yale.edu>
2014-10-05 00:46:40 -0400

    testing for sellign

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, FileField, IntegerField
 from wtforms.validators import DataRequired
 
 class BuyForm(Form):
-	name = StringField('File Name', validators=[DataRequired()])
+	# name = StringField('File Name', validators=[DataRequired()])
 	dropboxFile = FileField('File Attachment', validators=[DataRequired()])
 
 class SellForm(Form):
